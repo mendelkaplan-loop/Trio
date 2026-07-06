ddbe6a308307f652cae50937b9fde0ec468a39a2
dnzxy <48965855+dnzxy@users.noreply.github.com>
2026-07-06 21:45:23 +0200

    Recognize chart double-tap alongside the drag gesture

--- Trio/Sources/Modules/Home/View/Chart/MainChartView.swift
+++ Trio/Sources/Modules/Home/View/Chart/MainChartView.swift
@@ -148,7 +148,7 @@ struct MainChartView: View {
         .contentShape(Rectangle())
         .simultaneousGesture(panAndInspectGesture)
         .simultaneousGesture(magnifyGesture)
-        .onTapGesture(count: 2) { cycleZoomPreset() }
+        .simultaneousGesture(TapGesture(count: 2).onEnded { cycleZoomPreset() })
         .onDisappear {
             momentumTask?.cancel()
             inspectHoldTask?.cancel()
